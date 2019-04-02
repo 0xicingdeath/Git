fb1f7831e6f03c9484fc2df9f247b75bfdb69935
rw400 <ronniewong5@gmail.com>
2019-04-02 19:33:05 -0400

    initial

---
+++ contracts/Debug.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.5.6;
+
+contract Debug {
+
+    address public owner;
+
+    constructor() public {
+		owner = msg.sender;
+    }
+
+    // function setY(uint8 x) public {
+        
+    // }
+}
