9833cf465e4f2fdce590d2274cfe5b1159427fe6
rw400 <ronniewong5@gmail.com>
2019-04-02 19:38:55 -0400

    get and set

--- contracts/Debug.sol
+++ contracts/Debug.sol
@@ -3,12 +3,19 @@ pragma solidity 0.5.6;
 contract Debug {
 
     address public owner;
+    uint256 public amount;
 
     constructor() public {
 		owner = msg.sender;
     }
 
-    // function setY(uint8 x) public {
-        
-    // }
+    function getAmount () public view returns (uint256){
+        return amount;
+    }
+    
+    function setAmount (uint8 _amount) public {
+        amount = _amount;
+
+    }
+    
 }
