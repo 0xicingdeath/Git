30beefd2d205af39db9603348b25e452423830cb
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-04-02 21:53:45 -0400

    Remove folder.

--- Debugging/embarkArtifacts/contracts/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-"ERC20Interface": require('./ERC20Interface').default,
-"SafeMath": require('./SafeMath').default,
-"Auction": require('./Auction').default,
-"TokenLoanStorage": require('./TokenLoanStorage').default,
-"Collateral": require('./Collateral').default,
-"TokenLoan": require('./TokenLoan').default,
-
-};
